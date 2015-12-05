a001f0c13b06ef5a24f1bf630dda3a38ae8d8c5f
Mike Darwish <michael@darwish.ca>
2015-12-05 15:39:45 -0800

    Super-beautiful code to responsivize the search bar.

--- includes/templates/partials/search-bar.php
+++ includes/templates/partials/search-bar.php
@@ -2,11 +2,13 @@
 	<form action="search.php">
 		<div class="row">
 			<div class="form-group form-group-lg">
-				<div class="col-xs-10">
+				<div class="col-sm-10">
 					<input type="search" class="form-control" name="q" placeholder="What are you shopping for?" value="<?= h($q) ?>">
 				</div>
 
-				<div class="col-xs-2">
+				<div class="hidden-sm hidden-md hidden-lg"><p></p></div>
+
+				<div class="col-sm-2">
 					<button type="submit" class="btn btn-primary btn-lg">Search</button>
 				</div>
 			</div>
