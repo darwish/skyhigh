bd43d853065bf3fe319f09c584f9ee1a0cd1cad2
Mike Darwish <michael@darwish.ca>
2015-12-05 21:14:46 -0800

    Added categories.

---
+++ utility/insert-categories.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . '/../includes/start.php';
+
+$categories = json_decode(file_get_contents(__DIR__ . "/../data/categories.json"), true);
+
+foreach ($categories as $parentName => $children) {
+	$parent = storeCategory($parentName);
+
+	foreach ($children as $childName) {
+		storeCategory($childName, $parent);
+	}
+}
+
+function storeCategory($name, \RedBeanPHP\OODBBean $parent = null) {
+	$category = R::dispense("category");
+	$category->name = $name;
+	if ($parent) {
+		$category->parent = $parent;
+	}
+	R::store($category);
+	return $category;
+}
