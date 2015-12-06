4636cc986d6848d2e77f559fd0f954e3166c8fb1
Mike Hoffman <hoffman@learningbird.com>
2015-12-06 01:48:45 -0500

    added items and shops to the database

--- includes/start.php
+++ includes/start.php
@@ -1,8 +1,10 @@
 <?php
+require __DIR__ . '/password.php';
 require __DIR__ . '/rb.php';
 require __DIR__ . '/util.php';
 require __DIR__ . '/models/user.php';
-
+require_once __DIR__ . '/generate-db.php';
+	
 setupErrorHandling();
 
 if (file_exists(__DIR__ . '/db.php')) {
@@ -11,4 +13,7 @@ if (file_exists(__DIR__ . '/db.php')) {
 	R::setup('mysql:host=localhost;dbname=skyhigh', 'root', 'admin');
 }
 
+if (!R::findOne('item')) {
+ 	generateDB();
+}
 session_start();
