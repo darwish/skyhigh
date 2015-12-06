e698f4c710eab180c6b14b799baa152f957eaff2
Mike Darwish <michael@darwish.ca>
2015-12-06 09:58:40 -0800

    Added icon.

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -49,6 +49,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
+				<a class="navbar-brand" href="index.php" style="padding: 9px;"><img src="img/pin-small.png" width="32"></a>
 				<a class="navbar-brand" href="index.php">NearBuy</a>
 			</div>
 			<div id="navbar" class="navbar-collapse collapse">
