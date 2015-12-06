f86e4d171e3d84686d8b33ae1ece9dd8cda09387
Roderic Day <roderic.day@gmail.com>
2015-12-05 21:43:58 -0800

    Padless map

--- app/map.php
+++ app/map.php
@@ -1,19 +1,19 @@
 <div class="container fill">
-	<div class="panel panel-default" style="height:500px">
+	<div class="panel panel-default">
 	<div class="panel-heading">Map</div>
-		<div class="panel-body">
-			<div id="map" style="height:500px"></div>
+		<div id="map-panel" class="panel-body">
+			<div id="map"></div>
 		</div>
 	</div>
  </div>
 
  <script>
  	var mapLat = 37.7833;
-	var mapLon = -122.4167;	
-	
+	var mapLon = -122.4167;
+
 	L.mapbox.accessToken = 'pk.eyJ1IjoiY3JvY29kb3lsZSIsImEiOiJjaWhpZzRlY2MwbXFqdGNsenRqZmxqMHBrIn0.7yc8ndkeNHCD1TxhFzwe6w';
 	var map = L.mapbox.map('map', 'mapbox.dark').setView([mapLat, mapLon], 16);
-		
+
 	var dealPoints = new Array();
 	for(i=0; i<10; i++){
 		for(j=0; j<10; j++){
@@ -27,7 +27,7 @@
 			dealPoints[i*10+j].id = 4523;
 		}
 	}
-	
+
 	for (i=0; i< dealPoints.length; i++){
 		addItem(dealPoints[i]);
 	}
@@ -39,22 +39,22 @@
 	});
 
   L.marker([mapLat,mapLon], {icon: userLocation}).addTo(map);
-	
-	
+
+
 	function addItem(item){
 		var itemIcon = L.AwesomeMarkers.icon({
 			icon: 'shopping-cart',
 			markerColor: 'blue'
 		});
-		
+
 		var options = {};
 		options.title = item.percentOff + '% off ' + item.itemName;
 		options.icon = itemIcon;
-		
+
 		mark = L.marker([item.lat, item.lon], options).addTo(map);
 		mark.bindPopup('<a href="item.php?q=' + item.id + '"><img src="' + item.imgUrl + '" /> &nbsp; ' + item.itemName + '</a>').openPopup();
 	}
-	
+
 	</script>
 
 	<div id='log'></div>
